c0ff914d6399593237d6c79878c025e3c54b47d1
cristajon <61107925+cristajon@users.noreply.github.com>
2024-02-22 20:16:32 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -1,5 +1,5 @@
 #include "pipex.h"
-void pipex(char *file1, char **commandds, char *file2, char *envp))
+void pipex(char *file1, char **commands, char *file2, char *envp))
 {
     int     pipes[2];
     pid_t   father;
@@ -10,15 +10,23 @@ void pipex(char *file1, char **commandds, char *file2, char *envp))
     if (father <= -1)
         error("ERROR the creation of child process was unsuccessful");
     if (father == 0)
-        ft_father();
+        ft_father(file1[0],commands[0],pipes,envp);
     child = fork();
     if (child <= -1)
         error("ERROR the creation of child was unsuccessful");
     if (child == 0)
-        ft_child();
+        ft_child(file1[1],commands[1],pipes,envp);
     close(pipes[1]);
     close(pipes[2]);
+}
 
-
+int ft_father(file1[0],commands[0],pipes,envp)
+{
     
+    return(0);
+}
+int ft_child(file1[1],commands[1],pipes,envp)
+{
+
+    return(0);
 }
