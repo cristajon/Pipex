a618e791bdccff491da3ad6d1b1d4d55624dc77f
cristajon <61107925+cristajon@users.noreply.github.com>
2024-02-29 20:31:13 +0100

    Update cosas2.c

--- cosas2.c
+++ cosas2.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-ft_get_path(char *command,char envp[])
+char	ft_get_path(char *command,char envp[])
 {  
   char  *path;
   int    i;
