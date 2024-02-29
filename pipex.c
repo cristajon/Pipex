379b571935ea7ab1b2f58a534649f697ac02b73f
cristajon <61107925+cristajon@users.noreply.github.com>
2024-02-29 20:16:26 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -20,18 +20,36 @@ void pipex(char *file1, char **commands, char *file2, char *envp))
     close(pipes[2]);
 }
 
-int ft_father(file1,commands[0],pipes,envp)
+int ft_father(char *file1,char **commands[0], int pipes, char *envp)
 {
+    char *c;
+    
     dup2(pipes[1], STDOUT_FILENO);
     close(pipes[0]);
     dup2(file1, STDIN_FILENO);
+    c = ft_split(commands[0], ' ');
+    ft_get_path(c, envp);
+    if (execve(path, c, envp) == -1)
+	{
+		perror("Error execve");
+		exit(1);
+	}
+    
     return(0);
 }
 int ft_child(file2,commands[1],pipes,envp)
 {
+    char *c;
     dup2(pipes[2],STDIN_FILENO);
     close(pipes[1]);
     dup2(file2, STDOUT_FILENO);
+    c = ft_split(commands[1], ' ');
+    ft_get_path(c, envp);
+    if (execve(path, c, envp) == -1)
+	{
+		perror("Error execve");
+		exit(1);
+	}
 
     return(0);
 }
